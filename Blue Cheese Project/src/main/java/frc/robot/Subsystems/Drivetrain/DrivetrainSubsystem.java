c56f2f3445dbc5c8b4d5fc74e88542b8899b336a
SpaceStudios <109873883+SpaceStudios@users.noreply.github.com>
2024-10-30 20:47:26 +0000

    Added Drive train subsystem

--- Blue Cheese Project/src/main/java/frc/robot/Subsystems/Drivetrain/DrivetrainSubsystem.java
+++ Blue Cheese Project/src/main/java/frc/robot/Subsystems/Drivetrain/DrivetrainSubsystem.java
@@ -2,15 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Subsystems.Shooter;
+package frc.robot.Subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterSubsystem extends SubsystemBase {
-  /** Creates a new ShooterSubsystem. */
-  public ShooterSubsystem() {
-     
-  }
+public class DrivetrainSubsystem extends SubsystemBase {
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {}
 
   @Override
   public void periodic() {
