12d5044a9992391baf2be50f5c31b330d54603a8
DepressedFrog12 <pranav.paati@gmail.com>
2024-10-30 17:11:06 -0400

    Added ShooterIO constants

--- Blue Cheese Project/src/main/java/frc/robot/Subsystems/Shooter/ShooterIO.java
+++ Blue Cheese Project/src/main/java/frc/robot/Subsystems/Shooter/ShooterIO.java
@@ -1,5 +1,17 @@
 package frc.robot.Subsystems.Shooter;
 
 public interface ShooterIO {
-    
+    public static class ShooterData {
+        public double shooterVelocity = 0.0;
+        public double feederVelocity = 0.0;
+
+        public double shooterTemp = 0.0;
+        public double feederTemp = 0.0;
+        
+        public double shooterAmps = 0.0;
+        public double feeederAmps = 0.0;
+
+        public double shooterOutput = 0.0;
+        public double feederOutput = 0.0;
+    }
 }
